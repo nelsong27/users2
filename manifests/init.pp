84b3e6aea9a0c9b45e0f49b1731a4be1d8b66c0b
student12 <noreply@puppet.com>
2019-10-31 10:56:24 -0400

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,14 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include users2
+class users2 {
+  user { 'nelson2':
+    ensure    => present,
+    home      => '/home',
+    password  => '*',
+    shell     => '/bin/bash',
+ }
+}
