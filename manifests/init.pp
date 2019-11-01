ee639bda13b839e8d30a1f85f2e2526014beeec8
nelsong27 <nelson.gomez@puppet.com>
2019-11-01 10:47:31 -0400

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,9 +6,10 @@
 #   include users2
 class users2 {
   user { 'nelson2':
-    ensure    => present,
-    home      => '/home',
-    password  => '*',
-    shell     => '/bin/bash',
+    ensure             => present,
+    home               => '/home',
+    password           => '*',
+    shell              => '/bin/bash',
+    password_max_age   => -1,
   }
 }
