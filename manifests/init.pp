166231acb4d35acc297eb471ddb0f00fc2271284
nelsong27 <nelson.gomez@puppet.com>
2020-03-23 14:28:13 -0400

    max pwd age 30

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,6 @@ class users2 {
     home               => '/home',
     password           => '*',
     shell              => '/bin/bash',
-    password_max_age   => 50,
+    password_max_age   => 30,
   }
 }
